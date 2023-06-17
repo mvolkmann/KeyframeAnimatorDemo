b964ea4fca4bb5c24b06615150246b84d86ca5c9
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-06-17 18:47:43 -0500

    Initial Commit

---
+++ KeyframeAnimatorDemo/KeyframeAnimatorDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyframeAnimatorDemoApp.swift
+//  KeyframeAnimatorDemo
+//
+//  Created by Mark Volkmann on 6/17/23.
+//
+
+import SwiftUI
+
+@main
+struct KeyframeAnimatorDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
